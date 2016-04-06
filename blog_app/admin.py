e448df977000a5db9a0dc7cb21cade25ab25b8a4
Giuseppe Tiberi <tiberiweb@gmail.com>
2016-04-06 10:52:25 +0100

    Added app/urls.py and basic.html files

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from .models import Post
 
 admin.site.register(Post)
+
+# With the line above we are registering the Post model.
+# placed in models.py
--- blog_app/views.py
+++ blog_app/views.py
@@ -3,6 +3,13 @@ from django.utils import timezone
 from .models import Post
 
 
-# def post_list:
-#     Post=Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-#     return render(request, "blogtests.html",{'posts': posts})
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, "blogtests.html", {'posts': posts})
+
+
+'''
+Django allows us to append strings to table fields that act as boolean operators.
+In our case, we are appending "__tle", which stands for "less-than-or-equal-to" (<=)
+to our "published_date" field.
+'''
