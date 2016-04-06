71b97912510868922fb57ed2f9856ece93a2cae4
Giuseppe Tiberi <tiberiweb@gmail.com>
2016-04-06 11:57:22 +0100

    Added blogtests.html file

--- py_django_blog/settings.py
+++ py_django_blog/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog_app'
+    'blog_app',
+    'disqus',
+    'django.contrib.sites'
 ]
 
 MIDDLEWARE_CLASSES = [
